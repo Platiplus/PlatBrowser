b62d1b218ba7716620e13397d17461f20e1e1995
Platiplus <platiplus@Platipluss-Mac-Pro.local>
2017-12-29 10:51:39 -0300

    Initial Commit

---
+++ PlatBrowser/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PlatBrowser
+//
+//  Created by Platiplus on 29/12/17.
+//  Copyright © 2017 Platiplus. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
